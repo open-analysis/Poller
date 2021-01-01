7cd0b3fb692955efba334d4ff10d35891eab9f48
open-analysis <41541186+open-analysis@users.noreply.github.com>
2021-01-01 14:58:59 -0600

    Reworking to more akin to the final product
    Changing how this script operates so that it creates buttons with more meaningful data. Still trying to find a way to read a local file. Or maybe need to find a way to get the file another way.

--- myExtension/popup/poller.js
+++ myExtension/popup/poller.js
@@ -7,27 +7,24 @@ numPeople = 4;
 // numPeople that  when clicked change to a different list of buttons
 function init()
 {
-	for (i= 0; i < numPeople; i++)
+	
+	read();
+	
+	createButton(numPeople, "personButton", "button", "Person's Name", '#8fd8ff');
+	
+	// adding the event listener to each of these buttons
+	for(i = 0; i < numPeople; i++)
 	{
-		var personButton = document.createElement('button');
-		personButton.id = "personButton";
-		personButton.class = "button";
-		personButton.innerHTML = "Person's Name";
-		personButton.style.background = '#8fd8ff';
-		
-		personButton.addEventListener("click", function() {
+		button_array[i].addEventListener("click", function() {
 			personButton.style.background = '#689fbd';
 			personButton.innerHTML = "Ayyyyy";
 			button_clicked();
 		});
+	}
 		
-		button_array[i] = personButton;
-	}
 	
-	for (i= 0; i < numPeople; i++)
-	{
-		divID.appendChild(button_array[i]);
-	}
+	//createButton(1, "personButton", "button", tmp[0][0], '#8fd8ff');
+	
 }
 
 // changes the current list of buttons to a new
@@ -45,29 +42,47 @@ function button_clicked()
 	var heading = document.getElementById('heading');
 	heading.innerHTML = "New List!";
 	
+	createButton(numPeople*2, "newButton", "button", "New Button!", '#ffffffff');
+	
+}
+
+/*
+	Creates a button using a specified ID, class, innerHTML, and background style.
+	Foregoes the creation of an event listener, meaning that it will have to be created 
+	outside of this function by going through button_array.
+*/
+function createButton(numBtns, b_id, b_class, b_text, b_bg)
+{
 	// create the new set of buttons
-	button_array = [];
-	for (i= 0; i < numPeople*2; i++)
+	for (i= 0; i < numBtns; i++)
 	{
 		var newButton = document.createElement('button');
-		newButton.id = "personButton";
-		newButton.class = "button";
-		newButton.innerHTML = "New Button!";
-		newButton.style.background = '#ffffffff';
-		
-		newButton.addEventListener("click", function() {
-			newButton.style.background = '#000000';
-			newButton.innerHTML = "Ayyyyy again";
-		});
+		newButton.id = b_id;
+		newButton.class = b_class;
+		newButton.innerHTML = b_text;
+		newButton.style.background = b_bg;
 		
 		button_array[i] = newButton;
 	}	
 	
 	// add the new buttons the screen
-	for (i= 0; i < numPeople*2; i++)
+	for (i= 0; i < numBtns; i++)
 	{
 		divID.appendChild(button_array[i]);
 	}
+}
+
+function read()
+{
+	const path = "D:/Dev/Python/poli/Poller/folders.txt";
+	
+	var output = document.createElement('p');
+	output.id = "output";
+	output.textContent = "I'm the output!";
+	divID.appendChild(output);
+	
+	// need to find a way to read a locla file
+
 }
 
 init();
