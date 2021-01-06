028dfb7ffb85d36b7e05bc81b66542dbc6518415
Josh C <41541186+open-analysis@users.noreply.github.com>
2021-01-05 19:46:14 -0600

    Read local text file with all names
    Got the script  to read a local text with all of the names (and number of names) and then creates buttons with those names.
    
    Next step is to then read the text files within that person's folder and create buttons with links, etc.
    
    Problem: Need to get this information from a server rather than a local text file. Should just have to change path to localhost (or the IP) and the path to the file(s).

--- myExtension/popup/poller.js
+++ myExtension/popup/poller.js
@@ -1,36 +1,20 @@
 var button_array = [];
 var divID = document.getElementById('popup-content');
 var i, numPeople;
-numPeople = 4;
+numPeople = 2;
 
 // init the popup with a number of buttons determined by 
 // numPeople that  when clicked change to a different list of buttons
 function init()
-{
-	
-	read();
-	
-	createButton(numPeople, "personButton", "button", "Person's Name", '#8fd8ff');
-	
-	// adding the event listener to each of these buttons
-	for(i = 0; i < numPeople; i++)
-	{
-		button_array[i].addEventListener("click", function() {
-			personButton.style.background = '#689fbd';
-			personButton.innerHTML = "Ayyyyy";
-			button_clicked();
-		});
-	}
-		
-	
-	//createButton(1, "personButton", "button", tmp[0][0], '#8fd8ff');
+{			
+	readNames();
 	
 }
 
 // changes the current list of buttons to a new
 // set of buttons that are different
-function button_clicked()
-{
+function button_clicked(name)
+{	
 	// clear the old set of buttons
 	for (i= 0; i < numPeople; i++)
 	{
@@ -41,8 +25,22 @@ function button_clicked()
 	// change the heading
 	var heading = document.getElementById('heading');
 	heading.innerHTML = "New List!";
-	
-	createButton(numPeople*2, "newButton", "button", "New Button!", '#ffffffff');
+
+	/*
+		This section creates a button with links to the article that's attached to the title.
+		The for loop is for the number of news sites/text files that are in each person's folder
+		Each section should say the news site's name
+		The number of buttons created should be the same as the number of links provided
+	*/
+	for(i = 0; i < 3; i++)
+	{
+		var section = document.createElement('p');
+		section.id = "newsSiteDiv";
+		section.textContent = name;
+		divID.appendChild(section);
+		
+		createButton(8, "linkButton", "button", "New Button!", '#ffffffff');
+	}
 	
 }
 
@@ -53,36 +51,127 @@ function button_clicked()
 */
 function createButton(numBtns, b_id, b_class, b_text, b_bg)
 {
-	// create the new set of buttons
-	for (i= 0; i < numBtns; i++)
+	// check if the input text is an array
+	if (Array.isArray(b_text))
 	{
-		var newButton = document.createElement('button');
-		newButton.id = b_id;
-		newButton.class = b_class;
-		newButton.innerHTML = b_text;
-		newButton.style.background = b_bg;
+		// create the new set of buttons
+		for (i= 0; i < numBtns; i++)
+		{
+			var newButton = document.createElement('button');
+			newButton.id = b_id;
+			newButton.class = b_class;
+			newButton.innerHTML = b_text[i];
+			newButton.style.background = b_bg;
+			
+			button_array[i] = newButton;
+		}	
 		
-		button_array[i] = newButton;
-	}	
-	
-	// add the new buttons the screen
-	for (i= 0; i < numBtns; i++)
+		// add the new buttons the screen
+		for (i= 0; i < numBtns; i++)
+		{
+			divID.appendChild(button_array[i]);
+		}
+	}
+	else 
 	{
-		divID.appendChild(button_array[i]);
+		// create the new set of buttons
+		for (i= 0; i < numBtns; i++)
+		{
+			var newButton = document.createElement('button');
+			newButton.id = b_id;
+			newButton.class = b_class;
+			newButton.innerHTML = b_text;
+			newButton.style.background = b_bg;
+			
+			button_array[i] = newButton;
+		}	
+		
+		// add the new buttons the screen
+		for (i= 0; i < numBtns; i++)
+		{
+			divID.appendChild(button_array[i]);
+		}
+	}
+	
+	// adding the event listener to each of these buttons
+	for(i = 0; i < numBtns; i++)
+	{
+		button_array[i].addEventListener("click", function() {
+			//this.style.background = '#689fbd';
+			button_clicked(this.textContent);
+		})
+	}
+	
+}
+
+function readNames()
+{
+	const path = "../folders.txt";
+	
+	/*
+	var output = document.createElement('p');
+	output.id = "output";
+	output.textContent = "I'm the output!";
+	divID.appendChild(output);
+	*/
+	
+	fetch(path).then(function(response)
+	{
+		return response.text()
+	}).then(function(text)
+	{
+		//return text;
+		addNameToButton(text);
+	}).catch(function(err)
+	{
+		console.log('Fetch problem: ' + err.message);
+	});
+}
+
+function addNameToButton(data)
+{
+	//var outputElem = document.getElementById('output');
+	
+	if (data)
+	{
+		let text = new Array();
+		
+		text = data.split('\n');
+		
+		numPeople = parseInt(text[0]);
+		
+		text.shift();
+		
+		//outputElem.textContent = numPeople;
+		
+		createButton(numPeople, "personButton", "button", text, '#8fd8ff');
+	}
+	else
+	{
+		outputElem.textContent = "Failed to get data";
 	}
 }
 
-function read()
+function readNewsSite(name)
 {
-	const path = "D:/Dev/Python/poli/Poller/folders.txt";
+	const path = "../ppl.txt" + name + "/cnn.txt";
 	
 	var output = document.createElement('p');
 	output.id = "output";
 	output.textContent = "I'm the output!";
 	divID.appendChild(output);
 	
-	// need to find a way to read a locla file
-
+	fetch(path).then(function(response)
+	{
+		return response.text()
+	}).then(function(text)
+	{
+		//return text;
+		output.textContent = text;
+	}).catch(function(err)
+	{
+		console.log('Fetch problem: ' + err.message);
+	});
 }
 
 init();
